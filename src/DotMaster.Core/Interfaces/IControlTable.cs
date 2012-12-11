003d6d1b097ea9e895f3d16ad931018159a380d1
Anton Rudeshko <anton@rudeshko.com>
2012-12-12 00:58:48 +0400

    Added I18n resources and IControlTable interface

---
+++ src/DotMaster.Core/Interfaces/IControlTable.cs
@@ -0,0 +1,11 @@
+﻿namespace DotMaster.Core.Interfaces
+{
+    /// <summary>
+    /// Позволяет указывать контрольную таблицу, в которую будут 
+    /// записываться выигравшие перекрёстные ссылки
+    /// </summary>
+    public interface IControlTable
+    {
+         
+    }
+}
