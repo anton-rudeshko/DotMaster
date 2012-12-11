dbc6f03f13bf9f076f74282d0789ff6130a7336d
Anton Rudeshko <anton@rudeshko.com>
2012-12-12 01:00:50 +0400

    Implementing trust reader & tests

---
+++ src/DotMaster.Core/Processing/TrustReader.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DotMaster.Core.Interfaces;
+
+namespace DotMaster.Core.Processing
+{
+    public class TrustReader
+    {
+        private static readonly IList<string> Ignored = new[] { "ObjKey", "Xrefs" }; // todo: may be use lambda
+        private static readonly Type BaseObjectInterfaceType = typeof (IBaseObject<,,>);
+
+        public IDictionary<PropertyInfo, ITrustStrategy> ReadTrustRulesFromCurrentDomain()
+        {
+            return ReadTrustRulesFrom(AppDomain.CurrentDomain);
+        }
+
+        public IDictionary<PropertyInfo, ITrustStrategy> ReadTrustRulesFrom(AppDomain domain)
+        {
+            return ReadTrustRulesFrom(domain.GetAssemblies());
+        }
+
+        public IDictionary<PropertyInfo, ITrustStrategy> ReadTrustRulesFrom(IEnumerable<Assembly> assemblies)
+        {
+            return ReadTrustRulesFrom(assemblies.SelectMany(MasteredTypes));
+        }
+
+        public IDictionary<PropertyInfo, ITrustStrategy> ReadTrustRulesFrom(IEnumerable<Type> types)
+        {
+            return types.SelectMany(ReadTrustRulesFrom).ToDictionary(r => r.Key, r => r.Value);
+        }
+
+        public IDictionary<PropertyInfo, ITrustStrategy> ReadTrustRulesFrom(Type type)
+        {
+            return type.GetProperties().Where(NotIgnored).ToDictionary(p => p, ReadTrustRulesFrom);
+        }
+
+        public ITrustStrategy ReadTrustRulesFrom(PropertyInfo propertyInfo)
+        {
+            var trustStrategyAttribute = propertyInfo.GetCustomAttributes(true).OfType<TrustStrategyAttribute>().FirstOrDefault();
+            return trustStrategyAttribute == null ? null : trustStrategyAttribute.GetStrategyInstance();
+        }
+
+        public IEnumerable<Type> MasteredTypes(Assembly assembly)
+        {
+            return assembly.GetTypes().Where(TypeIsMastered);
+        }
+
+        public bool TypeIsMastered(Type type)
+        {
+            return type.GetInterfaces().Any(DefinedFromBaseObject);
+        }
+
+        private bool DefinedFromBaseObject(Type type)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == BaseObjectInterfaceType;
+        }
+
+        private bool NotIgnored(PropertyInfo propertyInfo)
+        {
+            return !Ignored.Contains(propertyInfo.Name);
+        }
+    }
+
+    internal class TrustRule
+    {
+    }
+
+    public class TrustContainer
+    {
+    }
+}
---
+++ src/DotMaster.Tests/Processing/TrustReaderTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using DotMaster.Core.Interfaces;
+using DotMaster.Core.Processing;
+using NUnit.Framework;
+
+namespace DotMaster.Tests.Processing
+{
+    [TestFixture]
+    public class TrustReaderTest
+    {
+        private TrustReader reader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            reader = new TrustReader();
+        }
+
+        [Test]
+        public void NotMasteredTypes([Values(typeof(double), typeof(TrustReader))] Type type)
+        {
+            Assert.That(reader.TypeIsMastered(type), Is.False);
+        }
+
+        [Test]
+        public void MasteredTypes([Values(typeof(TestBO))] Type type)
+        {
+            Assert.That(reader.TypeIsMastered(type), Is.True);
+        }
+
+        [Test]
+        public void PropertyEquals()
+        {
+            var prop1 = typeof (A).GetProperty("MyProperty");
+            var prop2 = typeof (A).GetProperty("MyProperty");
+
+            Assert.That(prop1, Is.EqualTo(prop2));
+        }
+
+        [Test]
+        public void PropertyNotEquals()
+        {
+            var prop1 = typeof (A).GetProperty("MyProperty");
+            var prop2 = typeof (B).GetProperty("MyProperty");
+
+            Assert.That(prop1, Is.Not.EqualTo(prop2));
+        }
+
+        [Test]
+        public void ReadTrustStrategy()
+        {
+            var trustStrategy = reader.ReadTrustRulesFrom(typeof (C).GetProperty("MyProperty"));
+
+            Assert.That(trustStrategy, Is.Not.Null);
+            Assert.That(trustStrategy, Is.InstanceOf<ITrustStrategy>());
+            Assert.That(trustStrategy, Is.InstanceOf<TestTrustStrategy>());
+        }
+
+        private class A
+        {
+            public int MyProperty { get; set; }
+        }
+
+        private class B
+        {
+            public int MyProperty { get; set; }
+        }
+
+        private class C
+        {
+            [TrustStrategy(typeof(TestTrustStrategy))]
+            public int MyProperty { get; set; }
+        }
+    }
+
+    internal class TestTrustStrategy : ITrustStrategy
+    {
+    }
+}
