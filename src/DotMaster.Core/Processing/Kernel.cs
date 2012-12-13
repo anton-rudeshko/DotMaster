604538af47f7a1642af4909ba5248af4cf3a6770
Anton Rudeshko <anton@rudeshko.com>
2012-12-13 22:05:44 +0400

    Progress on kernel + tests

--- src/DotMaster.Core/Processing/Kernel.cs
+++ src/DotMaster.Core/Processing/Kernel.cs
@@ -1,23 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using DotMaster.Core.Interfaces;
+using DotMaster.Core.Trust;
 
 namespace DotMaster.Core.Processing
 {
     public class Kernel
     {
-        public Kernel()
-        {
-        }
+        public IMasterDataBase MasterDB { get; set; }
+        public ITrustStrategy DefaultTrustStrategy { get; set; }
 
-        public Kernel(IMasterDataBase masterDB)
+        protected IDictionary<PropertyInfo, ITrustStrategy> TrustStrategies { get; set; }
+        protected TrustReader TrustReader { get; set; }
+
+        public Kernel(IMasterDataBase masterDB = null, ITrustStrategy defaultTrustStrategy = null)
         {
             MasterDB = masterDB;
+            DefaultTrustStrategy = defaultTrustStrategy ?? new LastUpdateDateTrustStrategy();
+                // todo: default trust strategy
+
+            TrustStrategies = new Dictionary<PropertyInfo, ITrustStrategy>();
+            TrustReader = new TrustReader();
         }
 
-        public IMasterDataBase MasterDB { get; set; }
+        public void Initialize()
+        {
+            TrustStrategies = TrustReader.ReadTrustRulesFromCurrentDomain();
+        }
 
         public void RegisterDataProvider<TKey, TBase, TXref>(ISourceDataProvider<TKey, TBase, TXref> dataProvider)
-            where TBase : class, IBaseObject<TKey, TBase, TXref>
+            where TBase : class, IBaseObject<TKey, TBase, TXref>, new() 
             where TXref : class, ICrossReference<TKey, TBase, TXref>
         {
             if (dataProvider == null)
@@ -27,40 +42,95 @@ namespace DotMaster.Core.Processing
             dataProvider.OnData += Process<TKey, TBase, TXref>;
         }
 
-        private void Process<TKey, TBase, TXref>(TXref xref)
-            where TBase : class, IBaseObject<TKey, TBase, TXref>
+        public void Process<TKey, TBase, TXref>(TXref xref)
+            where TBase : class, IBaseObject<TKey, TBase, TXref>, new()
             where TXref : class, ICrossReference<TKey, TBase, TXref>
         {
-            // Ignore null xrefs
             if (xref == null)
             {
-                return;
+                throw new ArgumentNullException("xref");
             }
-            TXref presentXref;
-            var baseObject = TryGetXref<TKey, TBase, TXref>(xref, out presentXref) ? presentXref.BaseObject : null /*new TBase()*/;
-            UpdateBaseObjectFromXref<TKey, TBase, TXref>(baseObject, xref);
+            if (string.IsNullOrWhiteSpace(xref.Source))
+            {
+                throw new ArgumentException(I18n.XrefSourceIsEmpty, "xref");
+            }
+            if (string.IsNullOrWhiteSpace(xref.SourceKey))
+            {
+                throw new ArgumentException(I18n.XrefSourceKeyIsEmpty, "xref");
+            }
+
+            // Update present xref if any
+            var presentXref = MasterDB.QueryForXref<TKey, TBase, TXref>(xref.SourceKey, xref.Source);
+            TBase baseObject;
+            if (presentXref != null)
+            {
+                presentXref.ObjectData = xref.ObjectData;
+                presentXref.LastUpdate = xref.LastUpdate;
+                baseObject = presentXref.BaseObject;
+            }
+            else
+            {
+                baseObject = new TBase { Xrefs = new List<TXref> { xref } };
+            }
+
+            RecalculateTrust<TKey, TBase, TXref>(baseObject);
         }
 
-        private void UpdateBaseObjectFromXref<TKey, TBase, TXref>(TBase baseObject, TXref xref)
+        private void RecalculateTrust<TKey, TBase, TXref>(TBase baseObject)
             where TBase : class, IBaseObject<TKey, TBase, TXref>
             where TXref : class, ICrossReference<TKey, TBase, TXref>
         {
-            // calculate trust rules
+            Debug.Assert(baseObject != null);
+            Debug.Assert(baseObject.Xrefs != null);
+            Debug.Assert(baseObject.Xrefs.Count > 0);
+
+            var properties = typeof (TBase).GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public);
+            var objKeyProperty = typeof (IBaseObject<TKey, TBase, TXref>).GetProperty("ObjKey");
+
+            // todo: do not copy ObjKey and Xrefs
+            foreach (var property in properties)
+            {
+                Debug.WriteLine("Processing property " + property.Name);
+                var mostTrusted = baseObject.Xrefs[0];
+
+                foreach (var xref in baseObject.Xrefs)
+                {
+                    var trustStrategy = GetTrustStrategy<TKey, TBase, TXref>(property, xref.Source);
+                    // todo: calc trust
+                }
+
+                CopyValue<TKey, TBase, TXref>(property, from: mostTrusted.ObjectData, to: baseObject);
+            }
+
+            baseObject.LastUpdate = baseObject.Xrefs.Max(x => x.LastUpdate);
+
+            //            baseObject.LastUpdate = xref.LastUpdate;
+
             // todo: how to handle BO deletion?
-            // copy fields from xref.ObjectData to baseObject
+            // если нет траста, обновляем поле по LUD
+
+            MasterDB.Save<TKey, TBase, TXref>(baseObject);
+        }
+
+        private static void CopyValue<TKey, TBase, TXref>(PropertyInfo property, TBase @from, TBase to)
+            where TBase : class, IBaseObject<TKey, TBase, TXref>
+            where TXref : class, ICrossReference<TKey, TBase, TXref>
+        {
+            property.SetValue(to, property.GetValue(@from, null), null);
+        }
+
+        private ITrustStrategy GetTrustStrategy<TKey, TBase, TXref>(PropertyInfo property, string source)
+            where TBase : class, IBaseObject<TKey, TBase, TXref>
+            where TXref : class, ICrossReference<TKey, TBase, TXref>
+        {
+            Debug.Assert(source != null);
+
+            return TrustStrategies.ContainsKey(property) ? TrustStrategies[property] : DefaultTrustStrategy;
         }
 
         private TBase LoadBaseObjectFor<TKey, TBase, TXref>(TXref xref)
         {
             throw new NotImplementedException();
-        }
-
-        private bool TryGetXref<TKey, TBase, TXref>(TXref xref, out TXref presentXref)
-            where TBase : class, IBaseObject<TKey, TBase, TXref>
-            where TXref : class, ICrossReference<TKey, TBase, TXref>
-        {
-            presentXref = MasterDB.QueryForXref<TKey, TBase, TXref>(xref.SourceKey, xref.Source);
-            return presentXref == null;
         }
 
         private TBase CreateNewBase<TKey, TBase, TXref>(TXref xref)
---
+++ src/DotMaster.Tests/KernelTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using DotMaster.Core.Interfaces;
+using DotMaster.Core.Processing;
+using Moq;
+using NUnit.Framework;
+
+namespace DotMaster.Tests
+{
+    [TestFixture]
+    public class KernelTest
+    {
+
+        private Kernel kernel;
+
+        private TestProvider provider;
+        private Mock<IMasterDataBase> db;
+        private DateTime now;
+        private TestXref testXref;
+        private TestBO objectData;
+        private TestBO savedBO;
+
+        [SetUp]
+        public void SetUp()
+        {
+            db = new Mock<IMasterDataBase>();
+            provider = new TestProvider();
+            kernel = new Kernel(db.Object);
+            kernel.RegisterDataProvider(provider);
+
+            now = DateTime.Now;
+            objectData = new TestBO { MyProperty = "new" };
+            testXref = new TestXref { SourceKey = "123", ObjectData = objectData, LastUpdate = now };
+        }
+
+
+        [Test]
+        public void NewBaseObjectCase()
+        {
+            // Arrange
+            PrepareSavedItems();
+
+            // Act
+            provider.Provide(testXref);
+
+            // Verify
+            db.Verify(x => x.Save<long, TestBO, TestXref>(It.IsAny<TestBO>()));
+
+            // Assert
+            Assert.That(savedBO, Is.Not.Null);
+            Assert.That(savedBO.MyProperty, Is.EqualTo("new"));
+            Assert.That(savedBO.ObjKey, Is.EqualTo(0));
+            Assert.That(savedBO.LastUpdate, Is.EqualTo(now));
+            Assert.That(savedBO.Xrefs, Is.Not.Null);
+            Assert.That(savedBO.Xrefs.Count, Is.EqualTo(1));
+
+            var savedXref = savedBO.Xrefs[0];
+            Assert.That(savedXref, Is.Not.Null);
+            Assert.That(savedXref.Source, Is.EqualTo(provider.Source));
+            Assert.That(savedXref.ObjectData.MyProperty, Is.EqualTo("new"));
+            Assert.That(savedXref.ObjKey, Is.EqualTo(0));
+            Assert.That(savedXref.LastUpdate, Is.EqualTo(now));
+        }
+
+        [Test]
+        public void PresentBaseObjectCase()
+        {
+            // Arrange
+            var presentBO = new TestBO { MyProperty = "old", ObjKey = 123, LastUpdate = now };
+            var presentXref = new TestXref
+                {
+                    BaseObject = presentBO, 
+                    BaseObjKey = presentBO.ObjKey, 
+                    LastUpdate = now, 
+                    ObjKey = 5132, 
+                    Source = provider.Source, 
+                    SourceKey = testXref.SourceKey, ObjectData = new TestBO { MyProperty = "old" }
+                };
+            presentBO.Xrefs = new List<TestXref> { presentXref };
+            db.Setup(x => x.QueryForXref<long, TestBO, TestXref>(testXref.SourceKey, provider.Source)).Returns(presentXref);
+            var newTime = now.AddDays(1);
+            testXref.LastUpdate = newTime;
+
+            PrepareSavedItems();
+
+            // Act
+            provider.Provide(testXref);
+
+            // Verify
+            VerifySaved();
+
+            // Assert
+            Assert.That(savedBO, Is.Not.Null);
+            Assert.That(savedBO, Is.SameAs(presentBO));
+            Assert.That(savedBO.MyProperty, Is.EqualTo("new"));
+            Assert.That(savedBO.ObjKey, Is.EqualTo(123));
+            Assert.That(savedBO.LastUpdate, Is.EqualTo(newTime));
+            Assert.That(savedBO.Xrefs, Is.Not.Null);
+            Assert.That(savedBO.Xrefs.Count, Is.EqualTo(1));
+
+            var savedXref = savedBO.Xrefs[0];
+            Assert.That(savedXref, Is.Not.Null);
+            Assert.That(savedXref, Is.SameAs(presentXref));
+            Assert.That(savedXref.Source, Is.EqualTo(provider.Source));
+            Assert.That(savedXref.ObjectData.MyProperty, Is.EqualTo("new"));
+            Assert.That(savedXref.ObjKey, Is.EqualTo(5132));
+            Assert.That(savedXref.LastUpdate, Is.EqualTo(newTime));
+        }
+
+        private void VerifySaved()
+        {
+            db.Verify(x => x.Save<long, TestBO, TestXref>(It.IsAny<TestBO>()));
+        }
+
+        private void PrepareSavedItems()
+        {
+            savedBO = null;
+            db.Setup(c => c.Save<long, TestBO, TestXref>(It.IsAny<TestBO>())).Callback<TestBO>(obj => savedBO = obj);
+        }
+    }
+}
