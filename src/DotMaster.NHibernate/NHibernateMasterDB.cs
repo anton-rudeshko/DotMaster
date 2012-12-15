1b90a81a49cd7f280d1f5c2e8b816f33b1c5ec7f
Anton Rudeshko <anton@rudeshko.com>
2012-12-16 00:51:35 +0400

    Preparing integration test

--- src/DotMaster.NHibernate/NHibernateMasterDB.cs
+++ src/DotMaster.NHibernate/NHibernateMasterDB.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using DotMaster.Core;
 using DotMaster.Core.Model;
---
+++ src/DotMaster.Tests/ManyToOne/ManyToOneTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using DotMaster.Core;
+using DotMaster.NHibernate;
+using NHibernate;
+using NHibernate.Linq;
+using NUnit.Framework;
+
+namespace DotMaster.Tests.ManyToOne
+{
+    [TestFixture]
+    public class ManyToOneTest
+    {
+        private Kernel kernel;
+        private ISessionFactory sessionFactory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var testConfiguration = NHibernateTestHelper.CreateStudentConfiguration();
+            NHibernateTestHelper.ExportSchema(testConfiguration);
+
+            sessionFactory = testConfiguration.BuildSessionFactory();
+            var masterDB = new NHibernateMasterDB(sessionFactory);
+
+            kernel = new Kernel(masterDB);
+        }
+
+        [Test]
+        public void SimpleSave()
+        {
+            // Arrange
+            var xref = new StudentXref
+                {
+                    Source = "Somewhere",
+                    SourceKey = "123123",
+                    ObjectData = new Student { Name = "Hello MDM" }
+                };
+
+            // Act
+            kernel.Process<long, Student, StudentXref>(xref);
+
+            // Assert
+            var students = sessionFactory.GetCurrentSession().Query<Student>().ToList();
+
+            Assert.That(students, Is.Not.Null);
+            Assert.That(students.Count, Is.EqualTo(1));
+            Assert.That(students[0].Name, Is.EqualTo("Hello MDM"));
+        }
+    }
+}
