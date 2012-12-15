61e25a2ec771d18128c8b89f143e6966212e541f
Anton Rudeshko <anton@rudeshko.com>
2012-12-15 20:42:50 +0400

    Created extension for dictionary

---
+++ src/DotMaster.Core/Utils/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotMaster.Core.Utils
+{
+    public static class Extensions
+    {
+        public static Dictionary<TKey, TValue>
+            ToDictionaryIgnoringNullValue<TSource, TKey, TValue>(this IEnumerable<TSource> sources,
+                                                                 Func<TSource, TKey> keySelector,
+                                                                 Func<TSource, TValue> valueSelector)
+            where TValue : class
+        {
+            if (sources == null)
+            {
+                throw new ArgumentNullException("sources");
+            }
+            if (keySelector == null)
+            {
+                throw new ArgumentNullException("keySelector");
+            }
+            if (valueSelector == null)
+            {
+                throw new ArgumentNullException("valueSelector");
+            }
+
+            var result = new Dictionary<TKey, TValue>();
+            foreach (var source in sources)
+            {
+                var value = valueSelector(source);
+                if (value != null)
+                {
+                    result.Add(keySelector(source), value);
+                }
+            }
+            return result;
+        }
+    }
+}
