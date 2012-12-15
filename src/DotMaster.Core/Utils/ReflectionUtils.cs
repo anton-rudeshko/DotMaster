a6adb6149a33bf202af7e0a4f0c508571891ce87
Anton Rudeshko <anton@rudeshko.com>
2012-12-15 22:56:45 +0400

    Added new method to ReflectionUtils

--- src/DotMaster.Core/Utils/ReflectionUtils.cs
+++ src/DotMaster.Core/Utils/ReflectionUtils.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace DotMaster.Core.Utils
 {
-    static internal class ReflectionUtils
+    public static class ReflectionUtils
     {
-        public static PropertyInfo[] GetMasteredProperties(Type type) 
+        public static PropertyInfo[] GetMasteredProperties(Type type)
         {
             return type.GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public);
         }
@@ -13,6 +14,30 @@ namespace DotMaster.Core.Utils
         public static void Copy(PropertyInfo property, object @from, object to)
         {
             property.SetValue(to, property.GetValue(@from, null), null);
+        }
+
+        public static string NameOf<TSource>(Expression<Func<TSource, object>> selector)
+        {
+            var type = typeof (TSource);
+
+            var member = selector.Body as MemberExpression;
+            if (member == null)
+            {
+                throw new ArgumentException(string.Format("Expression '{0}' refers to a method, not a property.", selector));
+            }
+
+            var propInfo = member.Member as PropertyInfo;
+            if (propInfo == null)
+            {
+                throw new ArgumentException(string.Format("Expression '{0}' refers to a field, not a property.", selector));
+            }
+
+            if (type != propInfo.ReflectedType && type.IsSubclassOf(propInfo.ReflectedType))
+            {
+                throw new ArgumentException(string.Format("Expresion '{0}' refers to a property that is not from type {1}.", selector, type));
+            }
+
+            return propInfo.Name;
         }
     }
 }
---
+++ src/DotMaster.Tests/Utils/ReflectionUtilsTest.cs
@@ -0,0 +1,15 @@
+﻿using DotMaster.Core.Utils;
+using NUnit.Framework;
+
+namespace DotMaster.Tests.Utils
+{
+    [TestFixture]
+    public class ReflectionUtilsTest
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.That(ReflectionUtils.NameOf<TestBO>(x => x.MyProperty), Is.EqualTo("MyProperty"));
+        }
+    }
+}
